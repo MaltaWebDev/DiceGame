8189703093c224391b4013745ba6f5e8b83011cc
MaltaWebDev <MaltaWebDev@gmail.com>
2023-06-27 18:15:29 +0200

    Set scores to 0, add hidden class

--- script.js
+++ script.js
@@ -2,10 +2,15 @@
 
 const leftPlayer = document.querySelector('.player--0');
 const rightPlayer = document.querySelector('.player--1');
-const leftScore = document.querySelector('#player--0');
-const rightScore = document.querySelector('#player--1');
+
+const leftScore = document.getElementById('score--0');
+const rightScore = document.getElementById('score--1');
+
 const rollDiceBtn = document.querySelector('.btn--roll');
 const newGameBtn = document.querySelector('.btn--new');
 const holdBtn = document.querySelector('.btn--hold');
 let activePlayerLeft = true;
 let secretNumber = Math.trunc(Math.random() * 6 + 1);
+
+leftScore.textContent = 0;
+rightScore.textContent = 0;
